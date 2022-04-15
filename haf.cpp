b9afa363317be34c71ec2fb3a758e5eacaca9857
igb-p <dima12p@gmail.com>
2022-04-15 23:21:03 +0700

    Добавьте файлы проекта.

---
+++ haf.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int aski[256]; char x;
+    for (int i = 0; i < 256; i++) aski[i] = 0;
+    ifstream a("text.txt");
+    if (!a) { cout << "Ошибка открытия файла"; return 0; };
+
+    a >> x;
+    while (a) {
+        aski[(int)x]++; a >> x;
+    }
+
+    for (int i = 0; i < 256; i++) cout << aski[i] << ' ';
+
+    a.close();
+}
